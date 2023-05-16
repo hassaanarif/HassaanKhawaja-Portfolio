b9241dd73e7c2d100f0a9cba0379866ffe68e077
Hassaan <hassaana@joblogic.com>
2023-05-16 19:37:44 +0500

    Added the ability to submit contact form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,7 +1,28 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+
 function Contact() {
-	function handleFormSubmit() {
-		return;
+	const [formData, setFormData] = useState({
+		name: "",
+		email: "",
+		telephone: "",
+		message: "",
+	});
+
+	console.log(formData);
+	function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+		setFormData({ ...formData, [event.currentTarget.id]: event.currentTarget.value });
 	}
+
+	function handleFormSubmit(event: FormEvent<HTMLFormElement>) {
+		event.preventDefault();
+
+		const email = "hassaanarif.ha@gmail.com";
+		const subject = "Contact Form Submission";
+		const mailtoUrl = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(`${formData.message}`)}`;
+
+		window.location.href = mailtoUrl;
+	}
+
 	return (
 		<section id="contact" className="my-20 mx-5 md:mx-36">
 			<h1 className="text-center text-6xl font-extrabold bg-gradient-to-r from-orange-600 to-purple-600 bg-clip-text text-transparent">CONTACT</h1>
@@ -18,46 +39,54 @@ function Contact() {
 						autoComplete="off"
 						className=" px-3 py-3 text-black font-semibold rounded-md focus:outline-none focus:ring w-full ease-linear transition-all duration-100"
 						placeholder="Name"
+						value={formData.name}
+						onChange={handleInputChange}
 					/>
 				</div>
 				<div className="w-full  my-5 px-3">
-					<label className="block text-xs font-bold mb-2" htmlFor="name">
+					<label className="block text-xs font-bold mb-2" htmlFor="email">
 						EMAIL
 					</label>
 					<input
-						id="name"
+						id="email"
 						required
 						autoComplete="off"
 						type="email"
 						className=" px-3 py-3 text-black font-semibold rounded-md focus:outline-none focus:ring w-full ease-linear transition-all duration-100"
 						placeholder="Email"
+						value={formData.email}
+						onChange={handleInputChange}
 					/>
 				</div>
 				<div className="w-full my-5 px-3">
-					<label className="block text-xs font-bold mb-2" htmlFor="name">
+					<label className="block text-xs font-bold mb-2" htmlFor="telephone">
 						TELEPHONE
 					</label>
 					<input
-						id="name"
+						id="telephone"
 						required
 						autoComplete="off"
 						type="tel"
 						className=" px-3 py-3 text-black font-semibold rounded-md focus:outline-none focus:ring w-full ease-linear transition-all duration-100"
 						placeholder="Telephone"
+						value={formData.telephone}
+						onChange={handleInputChange}
 					/>
 				</div>
 				<div className="w-full my-5 px-3">
-					<label className="block text-xs font-bold mb-2" htmlFor="name">
+					<label className="block text-xs font-bold mb-2" htmlFor="message">
 						MESSAGE
 					</label>
 					<textarea
-						id="name"
+						id="message"
 						required
 						autoComplete="off"
 						maxLength={2000}
 						rows={6}
 						className="resize-none px-3 py-3 text-black font-semibold rounded-md focus:outline-none focus:ring w-full ease-linear transition-all duration-100"
 						placeholder="Message"
+						value={formData.message}
+						onChange={handleInputChange}
 					/>
 				</div>
 				<div className="w-full my-5 px-3 text-center">
