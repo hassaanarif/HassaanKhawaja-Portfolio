7db524fa9f5853f0adc671b082c5fdfcde2580b7
Hassaan <hassaana@joblogic.com>
2023-05-16 16:18:50 +0500

    Removed the unnecessary routing children

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,11 +1,23 @@
-import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Skills from "./components/Skills";
+import Certifications from "./components/Certifications";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 export default function Layout() {
 	return (
 		<div className="bg-gray-900 text-gray-300 relative">
 			<NavBar />
-			<Outlet />
+			<About />
+			<Experience />
+			<Skills />
+			<Certifications />
+			<Projects />
+			<Contact />
+			<Footer />
 		</div>
 	);
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Layout from "./Layout.tsx";
+import Layout from "./Layout";
 import "./index.css";
+import About from "./components/About";
 
 const router = createBrowserRouter([
 	{
