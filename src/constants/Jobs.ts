e8a394f976f60561ad0dbb2c5c62792e1c9b0cf9
Hassaan <hassaana@joblogic.com>
2024-01-19 01:33:40 +0500

    Updated Experience

--- src/constants/Jobs.ts
+++ src/constants/Jobs.ts
@@ -5,52 +5,51 @@ import Job from "../interfaces/Job";
 
 const jobs: Job[] = [
 	{
-		company: "Joblogic",
-		location: "Lahore, Pakistan (Remote)",
+		company: "Joblogic Pvt Ltd",
+		location: "Sydney, Australia (Remote)",
 		companyLogoURL: JL,
 		time: "Full-Time",
 		startDate: "2021-02-08",
 		positions: [
 			{
-				title: "Full-Stack Developer",
+				title: "Full Stack Developer",
 				content: `
-• Involved in the development of TrackerGo, a vital internal tool of Joblogic Pvt Ltd, that enabled streamlined management of customer data, finances, and onboarding journey using C#, ASP.NET Core, Entity Framework, and SQL Server
-<br />• Implemented intricate business logic for customer permission management, ensuring that sensitive data remained secure and accessible only to authorized individuals
-<br />• Created a responsive user interface using Bootstrap and jQuery, providing efficient navigation of customer data, chat history, and tickets
-<br />• Leveraging my expertise, the system underwent substantial optimization, resulting in a significant boost to user productivity and overall system effectiveness
-<br />• My refined frontend/UI development skills enabled me to craft a highly intuitive and visually appealing user interface, resulting in a substantial uptick in user productivity and satisfaction
-<br />• Provided guidance and mentorship to junior engineers, instilling best practices and reinforcing a culture of continuous improvement`,
+•	Developed a cloud-hosted Customer Relationship Management software, called TrackerGo, in-house, which led to the riddance of reliance of Joblogic on third party paid CRMs, hence cutting its annual expenditures by 27%. 
+<br/>•	Devised different optimization techniques to deal with large data throughput, resulting in quick navigation between pages, faster page loads, smooth animations, quick query executions and overall, 60% improvement in user experience.
+<br/>•	Integrated TrackerGo with Xero API, automating the invoicing process, and ensuring accurate synchronization between the two platforms, resulting in an 18% reduction in invoicing errors.
+<br/>•	Migrated legacy code from monolithic to microservice architecture, making it 55% more scalable and maintainable.
+<br/>•	Skills used were HTML, CSS, JavaScript, jQuery, Vue.js, C#, ASP.NET Core MVC, Entity Framework, SQL, Swagger, Microsoft Azure, Azure Functions, Azure DevOps, CI/CD Pipeline and Git.
+`,
 			},
 			{
-				title: "Software Engineer",
+				title: "Database Engineer",
 				content: `
-• Devised and executed innovative strategies for optimizing SQL queries and improving system efficiency, resulting in increased application responsiveness and smoother end-user experiences
-<br />• Employed third-party tools such as "Stimulsoft" to develop dynamic forms and dashboards that were seamlessly integrated with the Joblogic Application, enabling customers to effortlessly access critical business data and metrics
-<br />• Fostered positive relationships with clients through extensive interaction and diligent gathering of their requirements, utilizing comprehensive SQL scripts to craft custom dashboards, forms, and reports tailored to their specific needs
-<br />• Mentored and trained new team members on database and SQL optimization techniques, fostering a culture of continuous learning and skill development
-<br />• Collaborated with fellow software engineers to troubleshoot complex technical issues and ensure adherence to coding best practices, conducting comprehensive code reviews to maintain high standards of code quality and performance
-<br />• Maintained comprehensive documentation of software development processes, including design documents, user manuals, and technical specifications, enabling efficient knowledge transfer and promoting the maintenance of best practices
-<br />• Actively participated in team meetings and provided innovative suggestions for improving software development processes and maximizing team efficiency`,
+•	Maintained and optimized enterprise-level databases, resulting in 31% reduction in peak database usage, hence significantly reducing Joblogic’s annual Microsoft Azure’s subscription cost. 
+<br/>•	Optimized more than 500 legacy queries using different ingenious techniques, resulting in 75% decrease in query execution time.
+<br/>•	Wrote more than 200 complex SQL Queries and Stored Procedures, using uncommon coding techniques such as recursive queries, to gather and manipulate data to be used for creating bespoke reports and dashboards, as per the customers’ requirements.
+<br/>•	Created more than 60 bespoke Dashboards, resulting in generating around £30,000 revenue for Joblogic.
+<br/>•	Skills used were SQL, MongoDB, C#, jQuery, JavaScript, Python, Power BI, Tableau, Stimulsoft, Git and Microsoft Azure.
+`,
 			},
 		],
 	},
 
 	{
-		company: "Freelancing",
+		company: "Freelance",
 		companyLogoURL: Freelance,
 		time: "Part-Time",
-		location: "Lahore, Pakistan (Remote)",
+		location: "Sydney, Australia (Remote)",
 		startDate: "2020-11-10",
 		positions: [
 			{
-				title: "React Developer",
+				title: "Front End Developer",
 				content: `
-• Developed and maintained scalable and responsive web applications using ReactJS, Functional Components, and Redux that resulted in a significant improvement in the user experience and a boost in overall productivity
-<br />• Implemented efficient and reusable code that followed industry best practices, such as modular programming, DRY and SOLID principles, resulting in streamlined development processes, enhanced code robustness, and reduced maintenance costs
-<br />• Optimized application performance and page load times using advanced optimization techniques such as lazy loading, server-side rendering, and performance profiling tools, resulting in an increase in user engagement and retention rates
-<br />• Collaborated with cross-functional teams in agile development environments, employing JIRA and other agile methodologies to deliver high-quality products in a timely manner, resulting in positive feedback and high customer satisfaction
-<br />• Developed and integrated complex REST APIs with Node.js and MongoDB, utilizing industry-standard authentication and authorization techniques such as OAuth2 and JWT, resulting in a secure and efficient data exchange system
-<br />• Utilized Tailwind CSS, Bootstrap and Material UI to create visually appealing and user-friendly UI designs that significantly improved the application's aesthetics and usability, resulting in high user adoption rates and positive user feedback`,
+•	Worked closely with 10+ clients from concept to development to deployment, providing technical guidance on best practices for performance, security, and costs.
+<br/>•	Designed and developed a scalable e-commerce platform, that handled 2000+ simultaneous users, incorporating a user-friendly interface and seamless payment processing, utilizing clean, robust, and efficient coding pattern.
+<br/>•	Created a task management application with real-time collaboration features, enhancing team productivity and communication.
+<br/>•	Built a dynamic blog platform, that hosted 400+ articles, with content management capabilities, enabling easy publishing and editing of articles.
+<br/>•	Skills used were HTML, CSS, Tailwind CSS, JavaScript, TypeScript, ReactJS, Node.js, Next.js, MongoDB, Redux, Primsa.io, React Query and Vercel.
+`,
 			},
 		],
 	},
